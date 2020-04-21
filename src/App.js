cca28211db83efe0de7d80db8445a5eed6d98405
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2020-04-21 14:41:37 -0300

    modicicação css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Conversor from "./components/conversor"
@@ -9,19 +8,24 @@ class App extends Component {
   return (
     <div className="App">
       <h1>Conversor de moedas</h1>
+      
       <div className="linha">
       <Conversor moedaA="USD" moedaB="BRL"> </Conversor>
       <Conversor moedaA="BRL" moedaB="USD"> </Conversor>
       </div>
+
       <div className="linha">
       <Conversor moedaA="CAD" moedaB="BRL"> </Conversor>
       <Conversor moedaA="BRL" moedaB="CAD"> </Conversor>
+      
       </div>
+
       <div className="linha">
       <Conversor moedaA="EUR" moedaB="BRL"> </Conversor>
       <Conversor moedaA="BRL" moedaB="EUR"> </Conversor>
       </div>
     </div>
+    
   );
  }
 }
--- src/components/conversor.js
+++ src/components/conversor.js
@@ -39,8 +39,8 @@ constructor(props){
     return (
       <div className="conversor">
         <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-        <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
-        <input type="button" value="Converter" onClick= {this.converter}></input>
+        <input type="text" className="txt" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+        <input type="button" className="btn" value="Converter Moeda" onClick= {this.converter}></input>
         <h2>{this.state.moedaB_valor}</h2>
       </div>
     )
